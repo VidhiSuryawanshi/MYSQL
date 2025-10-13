20e66b0e61d726a925ffa79335bf9b587f17f057
VidhiSuryawanshi <suryawanshividhi6@gmail.com>
2025-10-14 01:40:05 +0530

    Time: 483 ms (93.68%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,13 +1,27 @@
-SELECT E.employee_id 
-FROM  Employees E
-LEFT JOIN Salaries S
-ON E.employee_id = S.employee_id 
-WHERE S.employee_id IS NULL
-UNION  
-SELECT S.employee_id 
-FROM  Employees E
-RIGHT JOIN Salaries S
-ON E.employee_id = S.employee_id
-WHERE E.employee_id IS NULL
+-- SELECT E.employee_id 
+-- FROM  Employees E
+-- LEFT JOIN Salaries S
+-- ON E.employee_id = S.employee_id 
+-- WHERE S.employee_id IS NULL
+-- UNION  
+-- SELECT S.employee_id 
+-- FROM  Employees E
+-- RIGHT JOIN Salaries S
+-- ON E.employee_id = S.employee_id
+-- WHERE E.employee_id IS NULL
 
-ORDER BY  employee_id ASC;
+-- ORDER BY  employee_id ASC;
+
+SELECT employee_id 
+FROM Employees 
+LEFT JOIN Salaries 
+USING(employee_id)
+WHERE salary IS NULL
+UNION 
+SELECT employee_id 
+FROM Salaries
+LEFT JOIN Employees 
+USING(employee_id)
+WHERE name IS NULL
+
+ORDER BY employee_id ASC;
