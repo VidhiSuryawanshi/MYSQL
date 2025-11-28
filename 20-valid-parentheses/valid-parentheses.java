3d430afec2378edf2b8b20eac03b0dd602e2b760
VidhiSuryawanshi <suryawanshividhi6@gmail.com>
2025-11-28 21:33:17 +0530

    Time: 3 ms (71.41%) | Memory: 42.5 MB (44.69%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> str = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch=='(' || ch=='{' || ch=='['){
+                str.push(ch);
+            }
+            else{
+                if(str.isEmpty()){
+                    return false;
+                }
+
+                char top = str.pop();
+
+                if((ch==')' && top!='(' )|| 
+                   (ch=='}' && top!='{')||
+                   (ch==']' && top!='[')){
+                    return false;
+                }
+            }
+        }
+        return str.isEmpty();
+    }
+}
